a673e2940d5c1535cb32fcd7fc22aaa6b5b3610f
jeremymtan <jemtan@ucsc.edu>
2023-10-05 03:04:59 -0400

    change to databricks

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ import argparse
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import (
-    update_record,
-    delete_record,
-    create_record,
     general_query,
-    read_data,
 )
 
 
@@ -29,30 +25,9 @@ def handle_arguments(args):
     )
     args = parser.parse_args(args[:1])
     print(args.action)
-    if args.action == "update_record":
-        parser.add_argument("record_id", type=int)
-        parser.add_argument("server")
-        parser.add_argument("seconds_before_next_point", type=int)
-        parser.add_argument("day")
-        parser.add_argument("opponent")
-        parser.add_argument("game_score")
-        parser.add_argument("sets", type=int)
-        parser.add_argument("game")
-
-    if args.action == "create_record":
-        parser.add_argument("server")
-        parser.add_argument("seconds_before_next_point", type=int)
-        parser.add_argument("day")
-        parser.add_argument("opponent")
-        parser.add_argument("game_score")
-        parser.add_argument("sets", type=int)
-        parser.add_argument("game")
 
     if args.action == "general_query":
         parser.add_argument("query")
-
-    if args.action == "delete_record":
-        parser.add_argument("record_id", type=int)
 
     # parse again with ever
     return parser.parse_args(sys.argv[1:])
@@ -68,34 +43,9 @@ def main():
     elif args.action == "transform_load":
         print("Transforming data...")
         load()
-    elif args.action == "update_record":
-        update_record(
-            args.record_id,
-            args.server,
-            args.seconds_before_next_point,
-            args.day,
-            args.opponent,
-            args.game_score,
-            args.sets,
-            args.game,
-        )
-    elif args.action == "delete_record":
-        delete_record(args.record_id)
-    elif args.action == "create_record":
-        create_record(
-            args.server,
-            args.seconds_before_next_point,
-            args.day,
-            args.opponent,
-            args.game_score,
-            args.sets,
-            args.game,
-        )
     elif args.action == "general_query":
         general_query(args.query)
-    elif args.action == "read_data":
-        data = read_data()
-        print(data)
+
     else:
         print(f"Unknown action: {args.action}")
 
--- test_main.py
+++ test_main.py
@@ -30,62 +30,6 @@ def test_transform_load():
     assert "Transforming data..." in result.stdout
 
 
-def test_update_record():
-    """tests update_record"""
-    result = subprocess.run(
-        [
-            "python",
-            "main.py",
-            "update_record",
-            "1",
-            "John Doe",
-            "40",
-            "2023-09-05",
-            "Jane Doe",
-            "30-40",
-            "1",
-            "0-0",
-        ],
-        capture_output=True,
-        text=True,
-        check=True,
-    )
-    assert result.returncode == 0
-
-
-def test_delete_record():
-    """tests delete_record"""
-    result = subprocess.run(
-        ["python", "main.py", "delete_record", "1"],
-        capture_output=True,
-        text=True,
-        check=True,
-    )
-    assert result.returncode == 0
-
-
-def test_create_record():
-    """tests create_record"""
-    result = subprocess.run(
-        [
-            "python",
-            "main.py",
-            "create_record",
-            "John Doe",
-            "40",
-            "2023-09-05",
-            "Jane Doe",
-            "30-40",
-            "1",
-            "0-0",
-        ],
-        capture_output=True,
-        text=True,
-        check=True,
-    )
-    assert result.returncode == 0
-
-
 def test_general_query():
     """tests general_query"""
     result = subprocess.run(
@@ -93,19 +37,15 @@ def test_general_query():
             "python",
             "main.py",
             "general_query",
-            "SELECT * FROM ServeTimesDB WHERE server = 'John Doe'",
+            """SELECT t1.server, t1.opponent,
+                AVG(t1.seconds_before_next_point) as avg_seconds_before_next_point,
+                COUNT(*) as total_matches_played
+            FROM default.servetimesdb t1
+            JOIN default.eventtimesdb t2 ON t1.id = t2.id
+            GROUP BY t1.server, t1.opponent
+            ORDER BY total_matches_played DESC
+            LIMIT 10""",
         ],
-        capture_output=True,
-        text=True,
-        check=True,
-    )
-    assert result.returncode == 0
-
-
-def test_read_data():
-    """tests read_data"""
-    result = subprocess.run(
-        ["python", "main.py", "read_data"],
         capture_output=True,
         text=True,
         check=True,
@@ -116,8 +56,4 @@ def test_read_data():
 if __name__ == "__main__":
     test_extract()
     test_transform_load()
-    test_create_record()
-    test_read_data()
-    test_update_record()
-    test_delete_record()
     test_general_query()
